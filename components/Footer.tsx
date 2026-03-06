d639caf80c4295fdf7d32b76708377d022b7bdba
Nyasha Hama <nyashahama45@gmail.com>
2026-03-06 06:05:51 +0200

    feat: add Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="footer-logo">InvoiceChaser</div>
+      <div className="footer-copy">
+        © 2026 InvoiceChaser. All rights reserved.
+      </div>
+    </footer>
+  );
+}
